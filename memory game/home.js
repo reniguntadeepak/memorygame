b9b2769c830e4a1de55176d63c52598e21c9361b
Deepak <112082332+reniguntadeepak@users.noreply.github.com>
2023-02-13 01:17:45 +0530

    Add files via upload

---
+++ memory game/home.js
@@ -0,0 +1,6 @@
+function start() {
+    const square = document.createElement("div");
+    square.setAttribute("id", 1);
+    let outer = document.querySelector(".outer");
+    outer.append(square);
+}
